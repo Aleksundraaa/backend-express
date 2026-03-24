2064c8daaa70a1d480cce7b45381bbdb4ace4b95
Aleksundraaa <ladycaxap@gmail.com>
2026-03-24 18:57:06 +0500

    пункт 3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send({
+    items: [{
+      "id": 1,
+      "name": "Рома"
+    },
+      {
+        "id": 2,
+        "name": "Саша"
+      }]
+  });
 });
 
 module.exports = router;
