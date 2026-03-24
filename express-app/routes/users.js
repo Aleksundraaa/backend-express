3a368168e0ee5fe9b616a5570fd430ca71390234
slanets11 <romanmotyhlaev@gmail.com>
2026-03-24 20:12:18 +0500

    8

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,41 +1,40 @@
 const express = require('express');
 const router = express.Router();
-
-const userList = [
-    {"id": 1, "name": "Рома"},
-    {"id": 2, "name": "Саша"}]
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name text)`);
 
 
 router.get('/', function (req, res, next) {
-    res.send({
-        items: [{
-            "id": 1,
-            "name": "Рома"
-        },
-            {
-                "id": 2,
-                "name": "Саша"
-            }]
-    });
+    db.all("SELECT id, name FROM users", [], (err, rows) => {
+   if (err) {
+      console.log(err);
+   } else {
+      res.send(rows);
+   }
 });
+    });
 
 router.post('/', function (req, res, next) {
-    let newUserReq = req.body;
-    let newUser = {
-        "id": newUserReq.id,
-        "name": newUserReq.name,
-    }
-    res.status(201).json(newUser);
+    let name = req.body.name;
+    const insert = "INSERT INTO users (name) VALUES (?)";
+    db.run(insert, [name]);
+    res.status(201).json({name: name});
 })
 
+
 router.get('/:id', function (req, res, next) {
+    
     let userId = parseInt(req.params.id);
-    let userFind = userList.find(user => user.id === userId)
-    if (!userFind) {
-        return res.status(404)
-    } else {
-        res.status(200).json(userFind);
-    }
+    db.get("SELECT id, name FROM users WHERE id = ? ",  [], (err, rows) => {
+   if (err) {
+      console.log(err);
+   } else {
+      res.send(rows);
+   });
+
 })
 
 module.exports = router;
