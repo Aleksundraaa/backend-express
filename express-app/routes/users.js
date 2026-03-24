3a9bd3873feb6e46e9b73cea5fe0c71add8822fc
slanets11 <romanmotyhlaev@gmail.com>
2026-03-24 19:44:09 +0500

    6

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send({
-    items: [{
-      "id": 1,
-      "name": "Рома"
-    },
-      {
-        "id": 2,
-        "name": "Саша"
-      }]
-  });
+const userList = [
+    {"id": 1, "name": "Рома"},
+    {"id": 2, "name": "Саша"}]
+
+
+router.get('/', function (req, res, next) {
+    res.send({
+        items: [{
+            "id": 1,
+            "name": "Рома"
+        },
+            {
+                "id": 2,
+                "name": "Саша"
+            }]
+    });
 });
 
-router.post('/', function(req, res, next) {
-  let newUserReq = req.body;
-  let newUser = {
-    "id": newUserReq.id,
-    "name": newUserReq.name,
-  }
-  res.status(201).json(newUser);
+router.post('/', function (req, res, next) {
+    let newUserReq = req.body;
+    let newUser = {
+        "id": newUserReq.id,
+        "name": newUserReq.name,
+    }
+    res.status(201).json(newUser);
+})
+
+router.get('/:id', function (req, res, next) {
+    let userId = parseInt(req.params.id);
+    let userFind = userList.find(user => user.id === userId)
+    if (!userFind) {
+        return res.status(404)
+    } else {
+        res.status(200).json(userFind);
+    }
 })
 
 module.exports = router;
